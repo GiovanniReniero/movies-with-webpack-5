06704326519f231ef1ce842c3ddf048fa01d6c30
Giovanni Reniero <giovanni.reniero@gmail.com>
2021-05-12 10:58:48 +0200

    Added HTML generator

--- src/index.js
+++ src/index.js
@@ -1,7 +1,16 @@
 import _ from 'lodash';
 import './style.css';
-
+import printMe from './print.js';
 
 const insert = document.createElement("h1")
 insert.innerText = "Come on Gio , Kepeep on going"
 document.body .appendChild(insert)
+const btn = document.createElement('button');
+btn.innerHTML = 'Click me and check the console!';
+btn.addEventListener("click", printMe)
+document.body.appendChild(btn);
+
+
+
+
+// btn.onclick = printMe;
---
+++ src/print.js
@@ -0,0 +1,9 @@
+// const insert = document.createElement("h1")
+// insert.innerText = "Wohoo! from print me!"
+// document.body .appendChild(insert)
+
+export default function printMe() {
+  console.log("Wohoo!! from printMe")
+}
+
+// export { function printMe }
