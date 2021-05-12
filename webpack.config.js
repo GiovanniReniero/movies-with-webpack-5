b954e2ccdce7e0ba52641cb00c5092a2daa3f7b8
Giovanni Reniero <giovanni.reniero@gmail.com>
2021-05-12 11:16:35 +0200

    added webpack dev server

--- webpack.config.js
+++ webpack.config.js
@@ -2,15 +2,20 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
+  mode: 'development',
   entry: './src/index.js',
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist'),
     clean: true,
   },
+  devtool: 'inline-source-map',
+  devServer: {
+    contentBase: './dist',
+  },
   plugins: [
     new HtmlWebpackPlugin({
-      title: 'Output Management',
+      title: 'Devvelopment',
     }),
   ],
   module: {
